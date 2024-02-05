5aac5d25395b6503fa262eccb22f31a8292f4c98
mark <82868532+marksmart15@users.noreply.github.com>
2024-02-05 12:02:24 -0800

    added new interface and got the rest of the joints available

--- main_mp.py
+++ main_mp.py
@@ -1,24 +1,12 @@
 # main_mp.py
-from tkinter import messagebox
-from tkinter import ttk
 import os
 from utils import get_joint_combinations
 import cv2
 import mediapipe as mp
-import tkinter as tk
-from tkinter import Button, Label, Entry, Checkbutton, OptionMenu, StringVar, filedialog, Frame
 from pose_estimator_mp import PoseEstimatorMP
-
-class Main:
-    def __init__(self):
-        self.VI_M = VideoImgManager()
-
-    def img_estimation(self, img_path):
-        self.VI_M.estimate_img(img_path)
-
-    def live_estimation(self, chosen_joint, chosen_axis, videoType, save_data, save_location, file_name, data_points_per_second=None):
-        pose_estimator = PoseEstimatorMP(chosen_joint, chosen_axis, save_location, file_name, data_points_per_second)
-        self.VI_M.start_video_recording(pose_estimator, videoType, save_data)
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QComboBox, \
+    QPushButton, QCheckBox, QLineEdit, QErrorMessage
+from PyQt5.QtCore import Qt
 
 class VideoImgManager():
     def __init__(self):
@@ -65,137 +53,117 @@ class VideoImgManager():
         # Export data to CSV file if save_data is True
         if save_data:
             pose_estimator.export_data_to_csv()
-            
-class GUI(tk.Tk):
+
+
+class GUI(QWidget):
     def __init__(self, main_app):
         super().__init__()
-        self.title("Douglas Motion Capture")
-        self.geometry("400x600")
-        self.minsize(400, 400)  # Lock the minimum size of the window
-        self.maxsize(400, 400)  # Lock the maximum size of the window
-        self.configure(bg="grey")
 
         self.main_app = main_app
         self.save_location = ""
         self.file_name = ""
-        self.data_points_var = tk.StringVar()  # Variable to store the user input
+        self.data_points_var = ""
 
-        main_frame = Frame(self, bg="grey")
-        main_frame.pack(fill="both", expand=True)
+        self.init_ui()
 
-        # Dropdown menu for video device selection
-        self.label_device = Label(main_frame, text="Select Video Device:", font=("Arial", 14), bg="grey")
-        self.label_device.grid(row=0, column=0, pady=5)
-        self.device_var = StringVar(self)
+    def init_ui(self):
+        # Create widgets
+        self.label_device = QLabel("Select Video Device:")
+        self.device_combobox = QComboBox()
         video_devices = self.list_video_devices()
-        self.device_var.set(video_devices[0] if video_devices else "No video devices")
-        self.device_dropdown = OptionMenu(main_frame, self.device_var, *video_devices)
-        self.device_dropdown.grid(row=0, column=1, pady=5)
+        self.device_combobox.addItems(video_devices)
 
-        # Dropdown menu for joint selection
-        self.label_joint = Label(main_frame, text="Select Joint:", font=("Arial", 14), bg="grey")
-        self.label_joint.grid(row=1, column=0, pady=5)
-        self.joint_var = StringVar(self)
-        self.joint_var.set("LEFT_SHOULDER")  # Default joint
-        self.joint_options = ["LEFT_SHOULDER", "RIGHT_SHOULDER", "LEFT_ELBOW", "RIGHT_ELBOW"]  # Add more joints as needed
-        self.joint_dropdown = OptionMenu(main_frame, self.joint_var, *self.joint_options)
-        self.joint_dropdown.grid(row=1, column=1, pady=5)
+        self.label_joint = QLabel("Select Joint:")
+        self.joint_combobox = QComboBox()
+        self.joint_combobox.addItems(["LEFT_SHOULDER", "RIGHT_SHOULDER", "LEFT_ELBOW", "RIGHT_ELBOW", "RIGHT_WRIST", "LEFT_WRIST", "RIGHT_HIP", "LEFT_HIP", "RIGHT_KNEE", "LEFT_KNEE", "RIGHT_ANKLE", "LEFT_ANKLE"])
 
-        # Dropdown menu for axis selection
-        self.label_axis = Label(main_frame, text="Select Axis:", font=("Arial", 14), bg="grey")
-        self.label_axis.grid(row=2, column=0, pady=5)
-        self.axis_var = StringVar(self)
-        self.axis_var.set("X-axis")  # Default axis
-        self.axis_options = ["X-axis", "Y-axis"]  # Add more options if needed
-        self.axis_dropdown = OptionMenu(main_frame, self.axis_var, *self.axis_options)
-        self.axis_dropdown.grid(row=2, column=1, pady=5)
-        
-        # Checkbox for removing background 
-        self.remove_background_var = tk.IntVar()
-        self.remove_background_checkbox = Checkbutton(main_frame, text="Remove Background", variable=self.remove_background_var, font=("Arial", 12), bg="grey")
-        self.remove_background_checkbox.grid(row=3, column=0, pady=5, columnspan=3, sticky="w")
+        self.label_axis = QLabel("Select Axis:")
+        self.axis_combobox = QComboBox()
+        self.axis_combobox.addItems(["X-axis", "Y-axis"])
 
-        # Checkbox for data saving
-        self.save_data_var = tk.IntVar()
-        self.save_data_checkbox = Checkbutton(main_frame, text="Save Data", variable=self.save_data_var, font=("Arial", 12), bg="grey", command=self.toggle_save_options)
-        self.save_data_checkbox.grid(row=4, column=0, pady=5, columnspan=3, sticky="w")
+        self.remove_background_checkbox = QCheckBox("Remove Background")
 
-        # Entry for file name (initially hidden)
-        self.label_filename = Label(main_frame, text="Enter File Name:", font=("Arial", 14), bg="grey")
-        self.entry_filename = Entry(main_frame, font=("Arial", 12))
-        self.label_filename.grid(row=5, column=0, pady=5, sticky="w")
-        self.entry_filename.grid(row=5, column=1, pady=5, columnspan=2, sticky="ew")
-        self.label_filename.grid_remove()
-        self.entry_filename.grid_remove()
+        self.save_data_checkbox = QCheckBox("Save Data")
+        self.label_filename = QLabel("Enter File Name:")
+        self.entry_filename = QLineEdit()
+        self.label_data_points = QLabel("Data Points Per Second:")
+        self.entry_data_points = QLineEdit()
 
-        # Entry for data points per second (initially hidden)
-        self.label_data_points = Label(main_frame, text="Data Points Per Second:", font=("Arial", 14), bg="grey")
-        self.entry_data_points = Entry(main_frame, textvariable=self.data_points_var, font=("Arial", 12))
-        self.label_data_points.grid(row=6, column=0, pady=5, sticky="w")
-        self.entry_data_points.grid(row=6, column=1, pady=5, columnspan=2, sticky="ew")
-        self.label_data_points.grid_remove()
-        self.entry_data_points.grid_remove()
+        self.button_start = QPushButton("Start Estimation")
+        self.button_start.clicked.connect(self.start_estimation)
 
-        start_button_text = "Start Estimation"
-        self.button_start = Button(main_frame, text=start_button_text, command=self.start_estimation, font=("Arial", 12),
-                                   width=len(start_button_text))
-        self.button_start.grid(row=7, column=1, pady=10, columnspan=1, sticky="nsew")
+        # Layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.label_device)
+        layout.addWidget(self.device_combobox)
+        layout.addWidget(self.label_joint)
+        layout.addWidget(self.joint_combobox)
+        layout.addWidget(self.label_axis)
+        layout.addWidget(self.axis_combobox)
+        layout.addWidget(self.remove_background_checkbox)
+        layout.addWidget(self.save_data_checkbox)
+        layout.addWidget(self.label_filename)
+        layout.addWidget(self.entry_filename)
+        layout.addWidget(self.label_data_points)
+        layout.addWidget(self.entry_data_points)
+        layout.addWidget(self.button_start)
 
-        # Disable resizing of the window
-        self.resizable(False, False)
+        self.setLayout(layout)
 
-    def toggle_save_options(self):
-        # Toggle the visibility of the save options based on the save_data checkbox
-        if self.save_data_var.get():
-            self.label_filename.grid()
-            self.entry_filename.grid()
-            self.label_data_points.grid()
-            self.entry_data_points.grid()
-        else:
-            self.label_filename.grid_remove()
-            self.entry_filename.grid_remove()
-            self.label_data_points.grid_remove()
-            self.entry_data_points.grid_remove()
-        
-    
     def list_video_devices(self):
         devices = []
         for i in range(4):
             cap = cv2.VideoCapture(i)
             if cap.isOpened():
-                devices.append((f"Device {i}: {cap.get(cv2.CAP_PROP_FPS)} FPS", i))
+                devices.append(f"Device {i}: {cap.get(cv2.CAP_PROP_FPS)} FPS")
             cap.release()
         return devices
 
-
     def start_estimation(self):
-        # Get user inputs
-        chosen_joint = self.joint_var.get()
-        chosen_axis = self.axis_var.get().lower()
-        video_device = int(self.device_var.get().split(":")[0].split(" ")[-1])
-        save_data = self.save_data_var.get() == 1
-        file_name = self.entry_filename.get() or "output"
-        
-        # Validate data points per second if it's not empty
-        data_points_input = self.data_points_var.get()
-        data_points_per_second = int(data_points_input) if data_points_input.isdigit() and 1 <= int(data_points_input) <= 30 else None
+        chosen_joint = self.joint_combobox.currentText()
+        chosen_axis = self.axis_combobox.currentText().lower()
+        video_device = int(self.device_combobox.currentText().split(":")[0].split(" ")[-1])
+        save_data = self.save_data_checkbox.isChecked()
+        file_name = self.entry_filename.text() or "output"
+        data_points_input = self.entry_data_points.text()
+        data_points_per_second = int(data_points_input) if data_points_input.isdigit() and 1 <= int(
+            data_points_input) <= 30 else None
+
         if data_points_input and data_points_per_second is None:
-            messagebox.showerror("Error", "Data points per second must be a valid digit between 1 and 30.")
-            return  # Exit the function if validation fails
+            self.show_error("Data points per second must be a valid digit between 1 and 30.")
+            return
 
-        # Validate file name for Windows compatibility
         if not all(c not in r'\/:*?"<>|' for c in file_name):
-            messagebox.showerror("Error", "File name contains invalid characters.")
-            return  # Exit the function if validation fails
+            self.show_error("File name contains invalid characters.")
+            return
 
-        # Ensure the file name has the .csv extension
         if not file_name.endswith(".csv"):
             file_name += ".csv"
 
-        # Continue with the estimation
-        self.main_app.live_estimation(chosen_joint, chosen_axis, video_device, save_data, self.save_location, file_name, data_points_per_second)
+        self.main_app.live_estimation(chosen_joint, chosen_axis, video_device, save_data,
+                                      self.save_location, file_name, data_points_per_second)
 
-if __name__ == "__main__":
-    app = Main()
-    gui = GUI(app)
-    gui.mainloop()
+    def show_error(self, message):
+        error_dialog = QErrorMessage(self)
+        error_dialog.showMessage(message)
+
+
+class Main:
+    def __init__(self):
+        self.VI_M = VideoImgManager()
+
+    def live_estimation(self, chosen_joint, chosen_axis, video_type, save_data, save_location, file_name,
+                        data_points_per_second=None):
+        pose_estimator = PoseEstimatorMP(chosen_joint, chosen_axis, save_location, file_name,
+                                         data_points_per_second)
+        self.VI_M.start_video_recording(pose_estimator, video_type, save_data)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    main_app = Main()
+    gui = GUI(main_app)
+    gui.show()
+    app.exec_()
+    
+    
--- utils.py
+++ utils.py
@@ -9,6 +9,31 @@ def get_joint_combinations(chosen_joint):
         return "LEFT_SHOULDER", "LEFT_WRIST", "LEFT_ELBOW"
     elif chosen_joint == "RIGHT_ELBOW":
         return "RIGHT_SHOULDER", "RIGHT_WRIST", "RIGHT_ELBOW"
+    
+
+
+
+    elif chosen_joint == "RIGHT_WRIST":
+        return "RIGHT_ELBOW", "RIGHT_HAND", "RIGHT_WRIST"
+    elif chosen_joint == "LEFT_WRIST":
+        return "LEFT_ELBOW", "LEFT_HAND", "LEFT_WRIST"
+    
+    elif chosen_joint == "RIGHT_HIP":
+        return "RIGHT_SHOULDER", "RIGHT_KNEE", "RIGHT_HIP"
+    elif chosen_joint == "LEFT_HIP":
+        return "LEFT_SHOULDER", "LEFT_KNEE", "LEFT_HIP"
+
+    elif chosen_joint == "RIGHT_KNEE":
+        return "RIGHT_HIP", "RIGHT_ANKLE", "RIGHT_KNEE"
+    elif chosen_joint == "LEFT_KNEE":
+        return "LEFT_HIP", "LEFT_ANKLE", "LEFT_KNEE"
+    
+    elif chosen_joint == "RIGHT_ANKLE":
+        return "RIGHT_KNEE", "RIGHT_FOOT", "RIGHT_ANKLE"
+    elif chosen_joint == "LEFT_ANKLE":
+        return "LEFT_KNEE", "LEFT_FOOT", "LEFT_ANKLE"
+
+
     # Add more cases for other joints as needed
     else:
         return "LEFT_SHOULDER", "RIGHT_SHOULDER", "LEFT_ELBOW"
